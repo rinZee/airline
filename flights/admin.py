f5aa367a5caf604a290db154d351db9a99082b92
unknown <trex33160@gmail.com>
2021-08-29 22:24:26 +0545

    added passenger model and linked it to flight

--- flights/admin.py
+++ flights/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.admin.decorators import register
 
-from .models import Flight, Airport
+from .models import Flight, Airport, Passengers
 # Register your models here.
 admin.site.register(Airport)
 admin.site.register(Flight)
+admin.site.register(Passengers)
