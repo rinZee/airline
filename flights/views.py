c7c906ee070eded555ae469af7db4b574a525c7f
unknown <trex33160@gmail.com>
2021-08-29 22:12:26 +0545

    create a html file to view the description of an individual flights

--- flights/views.py
+++ flights/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 from .models import Flight
 # Create your views here.
+def flight(request, flight_id):
+	flight = Flight.objects.get(pk=flight_id)
+	return render(request, "flights/flight.html", {
+		"flight": flight
+	})
+
 def index(request):
 	return render(request, "flights/index.html", {
 		"flights": Flight.objects.all()
