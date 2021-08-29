b04751fbdf974783129463ee443276a1858cdb8d
unknown <trex33160@gmail.com>
2021-08-29 23:39:28 +0545

    added a POST method to add passenger to flights

--- flights/admin.py
+++ flights/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.contrib.admin.decorators import register
+from django.db import models
 
 from .models import Flight, Airport, Passengers
+
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+	list_display = ("id", "origin", "destination", "duration")
+
 admin.site.register(Airport)
-admin.site.register(Flight)
+admin.site.register(Flight, FlightAdmin)
 admin.site.register(Passengers)
